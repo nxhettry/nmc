79a3e05098045a9ffe48b2bba12ba6468d59135a
nxhettry <nishan.gtsn@gmail.com>
2024-12-12 15:46:58 +0545

    added sidebar

--- src/routes/Sidebar.jsx
+++ src/routes/Sidebar.jsx
@@ -1,31 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
+import { BiChevronDown, BiChevronUp } from "react-icons/bi";
 
 function Sidebar() {
+  const [selectedIndex, setSelectedIndex] = useState();
+  const items = [
+    { name: "Dashboard", Icon: false },
+    { name: "Budget", Icon: true },
+    { name: "Income", Icon: true },
+    { name: "Expenses", Icon: true },
+    { name: "Accounting", Icon: true },
+    { name: "Reports", Icon: true },
+  ];
   return (
-    <div className="h-screen py-8 w-1/5 bg-mainTheme text-white flex flex-col justify-between items-center">
-      <div className="flex flex-col gap-16">
+    <div className="h-screen py-8 w-[15%] bg-mainTheme text-white flex flex-col justify-between items-center">
+      <div className="flex flex-col gap-12 w-full">
         <img
           src="/logo512.png"
-          height={10000}
-          width={10000}
+          height={1000}
+          width={1000}
           alt="logo"
-          className="h-24 w-24 rounded-full border-2 border-yellow-300"
+          className="h-24 w-24 rounded-full mx-auto border-2 border-yellow-300"
         />
 
-        <div className="flex flex-col justify-center items-center gap-4">
-          {["Dashboard", "Income", "Expenses", "Budget", "Reports"].map(
-            (item) => {
-              return (
-                <div key={item} className="text-xl">
-                  {item}
-                </div>
-              );
-            }
-          )}
+        <div className="flex flex-col justify-center w-full items-start gap-2">
+          {items.map((item, index) => {
+            return (
+              <div
+                onClick={() => {
+                  parseInt(selectedIndex) === index
+                    ? setSelectedIndex("")
+                    : setSelectedIndex(`${index}`);
+                }}
+                key={item}
+                className="text-xl font-ubuntu px-12 py-1 w-full text-left flex justify-between items-center"
+              >
+                {item.name}
+                {item.Icon &&
+                  (parseInt(selectedIndex) === index ? (
+                    <BiChevronUp />
+                  ) : (
+                    <BiChevronDown />
+                  ))}
+              </div>
+            );
+          })}
         </div>
       </div>
 
-      <div className="w-4/5 mx-auto border border-gray-400 rounded-lg flex justify-center gap-12 items-center">
+      <div className="w-4/5 border border-gray-400 rounded-lg flex justify-start px-4 gap-3 items-center">
         <img
           src="logo192.png"
           alt="profile logo"
@@ -33,6 +55,7 @@ function Sidebar() {
           width={200}
           className="h-12 w-12 rounded-full"
         />
+        <div className="h-8 w-[1px] bg-white">&nbsp;</div>
         <p className="text-xl font-semibold">nishan</p>
       </div>
     </div>
